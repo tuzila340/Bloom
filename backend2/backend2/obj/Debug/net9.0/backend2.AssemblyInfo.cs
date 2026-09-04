f712710320543a45bf25091156f88aa746691e7d
tuzila <221207985+tuzila340@users.noreply.github.com>
2026-09-04 15:52:20 +0200

    1

--- backend2/backend2/obj/Debug/net9.0/backend2.AssemblyInfo.cs
+++ backend2/backend2/obj/Debug/net9.0/backend2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+219baa93bf2248b7fa51cd208333080985d1b7ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e53724820e20abead42177c2031167b2b2b95457")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
