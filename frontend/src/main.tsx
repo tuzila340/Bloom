f2cbd7869164a7365357ce948cb5a9e4acb2db3e
tuzila <221207985+tuzila340@users.noreply.github.com>
2026-09-07 17:56:15 +0200

    Add Registration page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
 import "./index.css";
 import WelcomePage from "./WelcomePage";
+import RegistrationPage from "./RegistrationPage";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <WelcomePage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/register" element={<RegistrationPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
