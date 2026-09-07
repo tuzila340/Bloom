a5d89653f79eeff506080a35b02c07318ffc0de2
tuzila <221207985+tuzila340@users.noreply.github.com>
2026-09-07 18:19:41 +0200

    made login page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import "./index.css";
 import WelcomePage from "./WelcomePage";
 import RegistrationPage from "./RegistrationPage";
+import LoginPage from "./LoginPage";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -11,6 +12,7 @@ createRoot(document.getElementById("root")!).render(
       <Routes>
         <Route path="/" element={<WelcomePage />} />
         <Route path="/register" element={<RegistrationPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
